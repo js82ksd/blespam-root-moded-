d900c47d5ca7b6e7249b210155737bfad953daa9
mr_ktoto228 <58346736+js82ksd@users.noreply.github.com>
2026-09-16 12:37:11 +0200

    Update MainActivity.kt

--- app/src/main/java/com/tutozz/blespam/MainActivity.kt
+++ app/src/main/java/com/tutozz/blespam/MainActivity.kt
@@ -491,7 +491,7 @@ class MainActivity : AppCompatActivity() {
         androidFastPairCircle = findViewById(R.id.androidFastPairCircle)
         samsungEasyPairBudsButton = findViewById(R.id.samsungEasyPairBudsButton)
         samsungEasyPairBudsCircle = findViewById(R.id.samsungEasyPairBudsCircle)
-        xiaomiQuickConnectButton = findViewById(R.id.xiaomiQuickConnectButton)
+        xiaomiQuickConnectButton = findViewById(R.id.XiaomiQuickConnectButton)
         xiaomiQuickConnectCircle = findViewById(R.id.XiaomiQuickConnectCircle)
         samsungEasyPairWatchButton = findViewById(R.id.samsungEasyPairWatchButton)
         samsungEasyPairWatchCircle = findViewById(R.id.samsungEasyPairWatchCircle)
